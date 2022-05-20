a1a8f33bcdafd015afee177142f6dba75f83b8ad
ange yen <ange.yen@fluxfederation.com>
2022-05-20 13:49:53 +1200

    format changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   root "urls#index"
 
   resources :urls
-  # get '/url', to: 'urls#show'
-  # post '/url', to: 'urls#create' 
+  # get '/url', to: 'urls#index'
+  # get '/url/:id', to: 'urls#show'
+
+  # get '/url/new', to: 'urls#new'
+  # post 'url', to: 'urls#create' 
+
+
   
   # get '/:short_url', to: redirect('/urls/%{long)url}')
 
   
-  # post '/urls/:id', to: 'urls#show'
+  # post '/url', to: 'urls#index'
 
 end
